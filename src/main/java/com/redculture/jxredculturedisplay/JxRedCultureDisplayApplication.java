0489c9e2d2da10a6698f79a58122b9b9644e117d
183 <balibalidu@gmail.com>
2025-12-17 16:59:20 +0800

    初始化项目代码

---
+++ src/main/java/com/redculture/jxredculturedisplay/JxRedCultureDisplayApplication.java
@@ -0,0 +1,13 @@
+package com.redculture.jxredculturedisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxRedCultureDisplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxRedCultureDisplayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redculture/jxredculturedisplay/JxRedCultureDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redculture.jxredculturedisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxRedCultureDisplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
