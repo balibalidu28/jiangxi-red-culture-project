ed6a9037f3dd0cf971ab5dd5cf4a6896d4faa927
unknown <wednesdayjaewoo@gmail.com>
2025-12-20 16:39:01 +0800

    红色圣地

--- src/main/java/com/redculture/jxredculturedisplay/repository/PartyEncyclopediaRepository.java
+++ src/main/java/com/redculture/jxredculturedisplay/repository/PartyEncyclopediaRepository.java
@@ -2,9 +2,17 @@ package com.redculture.jxredculturedisplay.repository;
 
 import com.redculture.jxredculturedisplay.model.PartyEncyclopedia;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
-public interface PartyEncyclopediaRepository extends JpaRepository<PartyEncyclopedia, Integer> {
-    // TODO(必须): List<PartyEncyclopedia> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(String kw1, String kw2);
+public interface PartyEncyclopediaRepository extends JpaRepository<PartyEncyclopedia, Long> {
+
+    // 原来的只搜标题
+    List<PartyEncyclopedia> findByTitleContaining(String keyword);
+
+    /**
+     * 【新增】高级搜索：同时搜标题 OR 内容
+     * IgnoreCase 表示忽略大小写（搜 "red" 能搜到 "Red"）
+     * 翻译成SQL: SELECT * FROM table WHERE lower(title) LIKE %kw% OR lower(content) LIKE %kw%
+     */
+    List<PartyEncyclopedia> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(String titleKw, String contentKw);
 }
