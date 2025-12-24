564b61669c342d4c6aeb489574f64ec93865128a
183 <balibalidu@gmail.com>
2025-12-24 18:15:49 +0800

    pom.xml

--- src/main/java/com/redculture/jxredculturedisplay/config/WebMvcConfig.java
+++ src/main/java/com/redculture/jxredculturedisplay/config/WebMvcConfig.java
@@ -8,8 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String heroPath = System.getProperty("user.dir") + "/uploads/images/hero/";
-        registry.addResourceHandler("/images/hero/**")
-                .addResourceLocations("file:" + heroPath);
+        String userPath = System.getProperty("user.dir") + "/uploads/images/user/";
+        registry.addResourceHandler("/images/user/**")
+                .addResourceLocations("file:" + userPath);
+
+        String storyPath = System.getProperty("user.dir") + "/uploads/images/story/";
+        registry.addResourceHandler("/images/story/**")
+                .addResourceLocations("file:" + storyPath);
     }
 }
