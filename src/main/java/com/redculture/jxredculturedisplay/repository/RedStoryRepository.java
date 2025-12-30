eb6444373bc53110cdf647f1ad8b77b604debc3b
wyydeyy <wyydeyy@outlook.com>
2025-12-30 20:35:09 +0800

    完成了红色英雄

--- src/main/java/com/redculture/jxredculturedisplay/repository/RedStoryRepository.java
+++ src/main/java/com/redculture/jxredculturedisplay/repository/RedStoryRepository.java
@@ -4,6 +4,7 @@ import com.redculture.jxredculturedisplay.model.RedStory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +17,13 @@ public interface RedStoryRepository extends JpaRepository<RedStory, Integer> {
 
     // 根据标题模糊查询（不分页）
     List<RedStory> findByTitleContainingIgnoreCase(String title);
+
+    // 根据英雄名称查找
+    List<RedStory> findByHeroNameContainingIgnoreCase(String heroName);
+
+    // 根据地点查找
+    List<RedStory> findByLocationContainingIgnoreCase(String location);
+
+    // 根据英雄名称和地点查找
+    List<RedStory> findByHeroNameContainingIgnoreCaseAndLocationContainingIgnoreCase(String heroName, String location);
 }
